26f66ff5d1473c1292bfcdbe1389acefdfb28b9f
shishkina <shishkina.kate@gmail.com>
2016-12-29 12:38:32 -0500

    added fish state to the App component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,27 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    //getInitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //update state
+    //... is spreading will take every item of the object(in this case state)
+    // and spread into the newly creating object
+    const fishes = {...this.state.fishes}
+    //add in new fish
+    const timestamp = Date.now();
+    fishes [`fish-${timestamp}`] = fish;
+    //set state
+    this.setState({ fishes });
+  }
   render(){
     return(
     <div className="catch-of-the-day">
@@ -11,7 +32,7 @@ class App extends React.Component {
         <Header tagline="Fresh Seafood Market"/>
       </div>
       <Order/>
-      <Inventory/>
+      <Inventory addFish={this.addFish} />
     </div>
     )
   }
