01f51c5eede04af9fcb16f191d77a86210460370
shishkina <shishkina.kate@gmail.com>
2016-12-29 12:47:42 -0500

    add sample fishes to state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     //getInitialState
     this.state = {
       fishes: {},
@@ -21,9 +23,15 @@ class App extends React.Component {
     const fishes = {...this.state.fishes}
     //add in new fish
     const timestamp = Date.now();
-    fishes [`fish-${timestamp}`] = fish;
+    fishes[`fish-${timestamp}`] = fish;
     //set state
     this.setState({ fishes });
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
   }
   render(){
     return(
@@ -32,7 +40,7 @@ class App extends React.Component {
         <Header tagline="Fresh Seafood Market"/>
       </div>
       <Order/>
-      <Inventory addFish={this.addFish} />
+      <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
     </div>
     )
   }
