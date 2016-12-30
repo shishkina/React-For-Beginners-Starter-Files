6ce697b51e0eb23a597669209670ceb31aa1fb81
shishkina <shishkina.kate@gmail.com>
2016-12-30 11:32:26 -0500

    successfully update inventory and state on change

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,6 +14,8 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+
     //getInitialState
     this.state = {
       fishes: {},
@@ -58,6 +60,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -94,10 +102,14 @@ class App extends React.Component {
       <Order
         fishes={this.state.fishes}
         order={this.state.order}
-        params={this.props.params}/>
+        params={this.props.params}
+        />
       <Inventory
         addFish={this.addFish}
-        loadSamples={this.loadSamples} />
+        loadSamples={this.loadSamples}
+        updateFish={this.updateFish}
+        fishes={this.state.fishes}
+         />
     </div>
     )
   }
